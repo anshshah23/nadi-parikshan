dd018c0a5ab60f0687b57fcef8594496ba11824c
anshshah23 <ANSH.SHAH082@svkmmumbai.onmicrosoft.com>
2024-12-02 00:50:09 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['some-module']  // Replace 'some-module' with the actual problematic module
-    }
-  },
 })
