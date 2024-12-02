f694684b8764edab8beec5d7b4f76ea927ec40a6
anshshah23 <ANSH.SHAH082@svkmmumbai.onmicrosoft.com>
2024-12-02 15:12:10 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Ensure this matches your app's deployment base URL
   build: {
     rollupOptions: {
       external: ['@react-google-maps/api'], // Add this line to externalize the module
